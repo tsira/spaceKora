25dafc4294b7bcd77d48b0aaf07a637c8301daa7
tsira <sira.tis@gmail.com>
2017-08-29 17:59:12 -0700

    initialCommit

---
+++ spaceKora/Assets/_scripts/playerGravityBody.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class playerGravityBody : MonoBehaviour {
+
+	public playerGravityAttractor attractor;
+	private Transform myTransform;
+
+	void Start () 
+	{
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+		myTransform = transform;
+	}
+
+	void FixedUpdate () 
+	{
+		if (attractor)
+		{
+			attractor.Attract(myTransform);
+		}
+	}
+}
